d1e38a4cc60eb5c64e5bb117e004f42c57f43b5c
MaksNyzhnov <niznovmaksim@gmail.com>
2023-09-19 01:05:10 +0300

    fixed contacts render after login

--- src/Pages/Contacts/Contacts.js
+++ src/Pages/Contacts/Contacts.js
@@ -1,13 +1,20 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getIsLoading } from 'redux/selectors';
 import css from './Contacts.module.css';
 import Section from 'components/Section/Section';
 import Form from 'components/Form/Form';
 import Filter from 'components/Filter/Filter';
 import ContactList from 'components/ContactList/ContactList';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
 
 const Contacts = () => {
+  const dispatch = useDispatch();
   const isLoading = useSelector(getIsLoading);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
   return (
     <Section title="Contacts">
       <div className={css.contacts_content}>
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://64ff6eebf8b9eeca9e2a1fde.mockapi.io';
-
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
